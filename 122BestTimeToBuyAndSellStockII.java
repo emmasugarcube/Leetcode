7616039fe3396cdce2d79a184697e35f66fa8f5a
emmasugarcube <emmasugarcube@gmail.com>
2017-09-07 19:15:36 -0700

    122BestTimeToBuyAndSellStockII

---
+++ 122BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            int diff = prices[i + 1] - prices[i];
+            if (diff > 0) {
+                maxProfit += diff;
+            }
+        }
+        return maxProfit;
+    }
+}
