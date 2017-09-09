808cdce74fa56c803a2e2322af70b179db001bd9
emmasugarcube <emmasugarcube@gmail.com>
2017-09-09 12:34:48 -0700

    75SortColors

---
+++ 75SortColors.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return;
+        }
+        
+        int pl = 0;
+        int pr = nums.length - 1;
+        int i = 0;
+        while (i <= pr) {
+            if (nums[i] == 0) {
+                swap(nums, pl, i);
+                pl++;
+                i++;
+            } else if (nums[i] == 1) {
+                i++;
+            } else {
+                swap(nums, pr, i);
+                pr--;
+            }
+        }
+    }
+    private void swap(int[] a, int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+}
