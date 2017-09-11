7726144abd2dc02bf042d77f9a1095ff9abd61fa
emmasugarcube <emmasugarcube@gmail.com>
2017-09-10 20:26:19 -0700

    71SimplifyPath

---
+++ 71SimplifyPath.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> s = new Stack<String>();
+        String[] p = path.split("/");
+        for (String t : p) {
+            if (!s.isEmpty() && t.equals("..")) {
+                s.pop();
+            } else if (!t.equals(".") && !t.equals("") && !t.equals("..")) {
+                s.push(t);
+            }
+        }
+        List<String> list = new ArrayList(s);
+        return "/" + String.join("/", list);
+    }
+}
